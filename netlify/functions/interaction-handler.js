f0ce908cb448e10fcfc3a682602b0ff27956ea59
Andrew Hinkson <andrewhinkson@andrews-mbp.home>
2024-03-18 14:50:02 -0400

    Moves threads within Slack

---
+++ netlify/functions/interaction-handler.js
@@ -0,0 +1,104 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  
+  const payload = JSON.parse(decodeURIComponent(event.body).replace('payload=', ''));
+  
+  
+  if (payload.type === 'view_submission' && payload.view.callback_id === 'move-thread-modal') {
+    const values = payload.view.state.values;
+    const threadLink = values.thread_link.input.value;
+    const newChannelID = payload.view.state.values.target_channel.select.selected_channel;
+
+    const confirmation = values.confirmation.checkbox.selected_options.length > 0;
+
+    
+    if (!confirmation || confirmation.length === 0) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          response_action: "errors",
+          errors: {
+            "confirmation_block": "You must confirm the action." 
+          }
+        })
+      };
+    }
+
+    
+    const linkParts = threadLink.split('/');
+    const channelID = linkParts[linkParts.indexOf('archives') + 1];
+    let timestamp = linkParts[linkParts.indexOf('archives') + 2].substring(1); 
+    timestamp = timestamp.slice(0, -6) + "." + timestamp.slice(-6); 
+    console.log(timestamp, channelID);
+
+    try {
+     
+      const repliesResponse = await axios.get('https://slack.com/api/conversations.replies', {
+        headers: {
+          Authorization: `Bearer ${SLACK_BOT_TOKEN}`, 
+        },
+        params: {
+          channel: channelID,
+          ts: timestamp
+        },
+      });
+  
+
+      if (repliesResponse.data.ok && repliesResponse.data.messages && repliesResponse.data.messages.length > 0) {
+        let threadTsInNewChannel = null; 
+        
+        for (const [index, message] of repliesResponse.data.messages.entries()) {
+          const postResponse = await axios.post('https://slack.com/api/chat.postMessage', {
+            channel: newChannelID,
+            text: message.text,
+            thread_ts: index === 0 ? undefined : threadTsInNewChannel, 
+            headers: {
+              Authorization: `Bearer ${SLACK_BOT_TOKEN}`, 
+              'Content-Type': 'application/json',
+            },
+          });
+      
+          if (postResponse.data.ok && index === 0) {
+            threadTsInNewChannel = postResponse.data.ts;
+          }
+      
+          
+          await new Promise(resolve => setTimeout(resolve, 200));
+        }
+
+      
+        for (const message of repliesResponse.data.messages) {
+          await axios.post('https://slack.com/api/chat.delete', {
+            channel: channelID,
+            ts: message.ts,
+          }, {
+            headers: {
+              Authorization: `Bearer ${SLACK_USER_TOKEN}`,
+              'Content-Type': 'application/json',
+            },
+          });
+        }
+      } else {
+        console.log("No messages found or unable to fetch thread.");
+      }
+
+      return {
+        statusCode: 200,
+        body: '',
+      };
+    } catch (error) {
+      console.error('Error moving thread:', error);
+      return {
+        statusCode: 500,
+        body: 'Failed to move thread',
+      };
+    }
+  } else {
+   
+    return {
+      statusCode: 200,
+      body: 'Unhandled event type',
+    };
+  }
+};
---
+++ netlify/functions/openmodal.js
@@ -0,0 +1,90 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  const params = new URLSearchParams(event.body);
+  const trigger_id = params.get('trigger_id');
+  const channel_id = params.get('channel_id'); 
+ console.log(event.body);
+  const modal = {
+    "type": "modal",
+    "callback_id": "move-thread-modal",
+    "title": {
+      "type": "plain_text",
+      "text": "Move Thread"
+    },
+    "submit": {
+      "type": "plain_text",
+      "text": "Submit"
+    },
+    "blocks": [
+      {
+        "type": "input",
+        "block_id": "thread_link",
+        "element": {
+          "type": "plain_text_input",
+          "action_id": "input",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Paste the link to the thread here"
+          }
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Thread Link"
+        }
+      },
+      {
+        "type": "input",
+        "block_id": "target_channel",
+        "element": {
+          "type": "channels_select",
+          "action_id": "select",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select the channel"
+          }
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Target Channel"
+        }
+      },
+      {
+        "type": "input",
+        "block_id": "confirmation",
+        "element": {
+          "type": "checkboxes",
+          "options": [
+            {
+              "text": {
+                "type": "mrkdwn",
+                "text": "I have informed the people in the thread I will be moving this thread"
+              },
+              "value": "confirmed"
+            }
+          ],
+          "action_id": "checkbox"
+        },
+        "label": {
+          "type": "plain_text",
+          "text": "Confirmation"
+        }
+      }
+    ]
+  };
+
+  await axios.post('https://slack.com/api/views.open', {
+    trigger_id,
+    view: modal
+  }, {
+    headers: {
+      'Authorization': `Bearer ${SLACK_BOT_TOKEN}`,
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return {
+    statusCode: 200,
+    body: ''
+  };
+};
